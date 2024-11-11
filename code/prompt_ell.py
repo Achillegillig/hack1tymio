0112f9147a9a410b45b0a1b73dcabea3e850051e
Achillegillig <120458968+Achillegillig@users.noreply.github.com>
2024-11-11 18:51:05 +0100

    update

--- code/prompt_ell.py
+++ code/prompt_ell.py
@@ -5,6 +5,7 @@ from openai import Client, OpenAI
 from typing import List
 import ell
 from ell import Message
+import time
 
 MODEL = "llama3.2:3b"
 
@@ -18,7 +19,7 @@ client = OpenAI(
 from pydantic import BaseModel, Field
 
 class TymioCommand(BaseModel):
-    command: str = Field(description="Command for the Tymio robot."
+    command: str = Field(description="Command for the Thymio robot."
                          
                         #  The command can be one of the following:
                         #  - 'forward': Move the robot forward.
@@ -30,7 +31,7 @@ class TymioCommand(BaseModel):
                          )
     duration: int = Field(description="Duration of the command in milliseconds.")
     communication: str = Field(description="Communication to send to the other bots")
-    thoughts: str = Field(description="Thoughts process of the decision")
+    thoughts: str = Field(description="Thought process of the decision")
 
 # @ell.simple(model=MODEL, client=client)
 # def write_a_poem(name: str, temperature=0.5):
@@ -50,11 +51,23 @@ def control_tymio(message_history: List[Message]) -> List[Message]:
     ] + message_history
 
 message_history = []
+
+initial_user_input = "My name is Tymiotée, I am a robot. We need to find a way to exit this maze. What do we do?"
+first_iter = True
 while True:
-    user_input = input("You: ")
+    if first_iter:
+        user_input = initial_user_input
+        first_iter = False
+        print("Tymiotee:", user_input)
+    else:
+        user_input = control_tymio(message_history)
+        print("Tymiotee:", user_input.text)
+        time.sleep(2)
+    # user_input = input("You: ")
     message_history.append(ell.user(user_input))
     output = control_tymio(message_history)
     response = output.text
     print("Bot:", response)
     # print("Command:", response.command)
-    message_history.append(output)
+    time.sleep(5)
+    message_history.append(ell.user(output))
