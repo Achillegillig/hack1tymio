b5130769a23f48fcf78d2afb1e80cefe81664417
Achillegillig <120458968+Achillegillig@users.noreply.github.com>
2024-11-17 12:03:24 +0100

    ITS WORKING MAN

--- Supervisor.py
+++ Supervisor.py
@@ -3,6 +3,31 @@ from styles import styles
 import streamlit as st
 import random
 import ell
+import time
+from thymiodirect import Thymio
+from thymiodirect.thymio_serial_ports import ThymioSerialPort
+
+speed = 100
+
+
+def init_thymio():
+    thymio_serial_ports = ThymioSerialPort.get_ports()
+    serial_port = thymio_serial_ports[0].device
+    th = Thymio(use_tcp=False,
+                    serial_port=serial_port,
+                    host=None,
+                    refreshing_coverage={
+                        "prox.horizontal",
+                        "button.center",
+                        "prox.ground.delta"
+                    },
+                   )
+    th.connect(delay=2)
+    for node in th.nodes():
+        print(node)
+    return th
+
+th = init_thymio()
 
 
 class Supervisor:
@@ -83,16 +108,86 @@ class Supervisor:
     
     def _run(self):
         # Start the game
-        print('Run the game !')
+        # print('Run the game !')
         
-        # Set title & styles of the page
-        st.title("Conversation entre agents LLM")
-        st.markdown(styles, unsafe_allow_html=True)
+        # # Set title & styles of the page
+        # st.title("Conversation entre agents LLM")
+        # st.markdown(styles, unsafe_allow_html=True)
 
         # While the game is running
         while True:
             # Launch the discussion
-            self._launch_discussion()
+            #self._launch_discussion()
+            # get orders from supervisor
+            # returns LEFT, RIGHT, UP, DOWN, STOP (référentiel de la carte)
+            # Interpret in robot space
+            #order_robot_space = "RIGHT"
+
+            self.play(th.first_node())
+            time.sleep(3)
+
+
             print('Discussion over. Press any key to continue...')
             input()
 
+    @staticmethod
+    def intersection(node_id, prox_left, prox_right):
+
+        ground_left = th[node_id]["prox.ground.delta"][0]
+        ground_right = th[node_id]["prox.ground.delta"][1]
+
+        thresh = 300
+        # Check if intersection has been reached
+        delta = max(prox_left - ground_left , prox_right - ground_right)
+        print("delta", delta , "ground_left", ground_left, "prox_left", prox_left, "ground_right", ground_right, "prox_right", prox_right)
+
+        if delta > thresh:
+            print("Intersection detected !")
+            # th.set_variable_observer(node_id, rotate) # a changer rihgt/left
+
+            # Avoid correction at intersection
+            th[node_id]["motor.left.target"] = speed
+            th[node_id]["motor.right.target"] = speed
+            time.sleep(1.9) # We need to wait a bit before rotating to get a good angle
+            th[node_id]["motor.left.target"] = 0
+            th[node_id]["motor.right.target"] = 0
+            return True
+        return False
+
+    def play(self, node_id):
+        th.set_variable_observer(node_id, line_behavior)
+        print("Playing ", node_id)
+
+        ground_left = th[node_id]["prox.ground.delta"][0]
+        ground_right = th[node_id]["prox.ground.delta"][1]
+
+
+        # TODO Take a decision from buffered values
+        # TODO cooldown intersection
+        while not self.intersection(node_id, ground_left, ground_right):
+            ground_left = th[node_id]["prox.ground.delta"][0]
+            ground_right = th[node_id]["prox.ground.delta"][1]
+            time.sleep(0.1)
+
+        th.set_variable_observer(node_id, lambda node_id: None)
+
+
+
+def line_behavior(node_id):
+    global done, prox_right, prox_left
+    max_steer = 10
+
+    ground_left = th[node_id]["prox.ground.delta"][0]
+    ground_right = th[node_id]["prox.ground.delta"][1]
+
+    # Set motor speed and correction
+    tmp = max_steer * ground_left - max_steer * ground_right
+    steerL = tmp // speed
+    th[node_id]["motor.left.target"] = speed + steerL
+    th[node_id]["motor.right.target"] = speed - steerL
+    print("Motor left", th[node_id]["motor.left.target"], "Motor right", th[node_id]["motor.right.target"] , "steerL", steerL)
+
+if __name__ == "__main__":
+    s = Supervisor(n_agents=5, size=(10,10))
+    s._run()
+    
